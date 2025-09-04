486ba299ab45f8ef3527bc4cc37e4fe667e38882
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2025-09-04 20:17:43 +0600

    nd commit

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(cookieParser())
 
 // global middlewares
 app.use(cors({
-    origin: process.env.FRONTEND_URL  || "http://localhost:5173",
-    credentials : true
+  origin: ["http://localhost:3000", "http://localhost:5173"],
+  credentials: true, // if you're sending cookies/auth headers
 }));
 
 app.use(express.json());
